3f6165e7018858a57def1ba3a1b7eb7810d9da0d
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-21 14:51:53 -0400

    Added USES_LIBRARIES property

--- GCamGOPrebuilt/Android.mk
+++ GCamGOPrebuilt/Android.mk
@@ -2,10 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := GCamGOPrebuilt
-LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := GCamGOPrebuilt.apk
-LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
 LOCAL_OVERRIDES_PACKAGES := Snap Camera2 Camera SnapCamera
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 include $(BUILD_PREBUILT)
--- device.mk
+++ device.mk
@@ -74,7 +74,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration.xml \
     $(DEVICE_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_policy_configuration.xml
 
 PRODUCT_PACKAGES += \
