d99eb595f5c0e5937fc207d97fc020a4526eca46
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-26 19:36:24 -0400

    Fixes

--- device.mk
+++ device.mk
@@ -63,7 +63,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/etc/fstab.mt6765:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6765
 
 # Prebuilt
-ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
+ifeq($(TARGET_FORCE_PREBUILT_KERNEL),true)
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/prebuilt/dtb.img:dtb.img
 endif
